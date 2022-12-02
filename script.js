786f0ab54197eccf1e19f8d65fcc332df53284d7
TheAmazingCabbage <81103015+TheAmazingCabbage@users.noreply.github.com>
2022-12-02 16:36:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let Error_1 = document.querySelector('h1');
 let count =7;
 setInterval(()=>{
-    Error_404.innerText = count;
+    Error_1.innerText = count;
     count++
     
     if(count > 5) location.replace('https://www.youtube.com/watch?v=iQKzuAhzoi0&t=50s')
