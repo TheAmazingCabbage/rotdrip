0627c93e75c8646ea6c36b47ce2724ebb75afaa7
TheAmazingCabbage <81103015+TheAmazingCabbage@users.noreply.github.com>
2022-12-02 16:12:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let counter = document.querySelector('h1');
-let count =61;
+let count =7;
 setInterval(()=>{
     counter.innerText = count;
     count++
     
     if(count > 5) location.replace('https://www.google.com/')
     
-},1000)
+},0001)
