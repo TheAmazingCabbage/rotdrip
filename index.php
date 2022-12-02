6d57d121dbcdb64fa7164fc3cb2ead35daf9f710
TheAmazingCabbage <81103015+TheAmazingCabbage@users.noreply.github.com>
2022-12-02 15:50:13 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+$url = 'https://example.com';
+if (isset($_GET['url']) && !empty($_GET['url'])) {
+	$url = urldecode($_GET['url']);
+} else if (isset($_GET['legacy_url']) && !empty($_GET['legacy_url'])) {
+	$url = $_GET['legacy_url'];
+}
+?>
+<html>
+	<head>
+	<meta id="redirect" http-equiv="refresh" content="0;URL='<?php echo htmlspecialchars($url, ENT_QUOTES, 'UTF-8'); ?>'" />
+	</head>
+</html>
